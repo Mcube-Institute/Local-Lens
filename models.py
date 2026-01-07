f675981dafdcf09498b498f874367a99c5c25171
Sri-Akash <sriakash152@gmail.com>
2026-01-07 18:39:51 +0530

    Models.py Completed

---
+++ models.py
@@ -0,0 +1,61 @@
+from mongoengine import *
+from uuid import uuid4
+from datetime import datetime
+
+class Role(Document):
+    id=StringField(primary_key=True, default= lambda: str(uuid4()))
+    name=StringField(required=True)
+    createdAt=DateTimeField(default=datetime.now())
+    updateAt=DateTimeField()
+
+class User(Document):
+    id=StringField(primary_key=True, default= lambda: str(uuid4()))
+    name=StringField(required=True)
+    email=EmailField(required=True,unique=True)
+    password=StringField(required=True)
+    role=ReferenceField(Role,required=True)
+    createdAt=DateTimeField(default=datetime.now())
+    updateAt=DateTimeField()
+
+class Location(Document):
+    id=StringField(primary_key=True, default= lambda: str(uuid4()))
+    street=StringField(required=True)
+    city=StringField(required=True)
+    state=StringField(required=True)
+    country=StringField(required=True)
+    pincode=StringField(required=True,min_length=6,max_length=6)
+    createdAt=DateTimeField(default=datetime.now())
+    updateAt=DateTimeField()
+
+class Issue(Document):
+    user=ReferenceField(User,required=True)
+    id=StringField(primary_key=True, default= lambda: str(uuid4()))
+    issueTittle=StringField(required=True)
+    issueDescription=StringField(required=True)
+    category=StringField(required=True,choices=["Road","Grbage","Water","Electricity","Others"])
+    location=ReferenceField(Location,required=True)
+    imagePath=URLField()
+    status=StringField(default='reported',choices=['reported','progress','resolved','rejected'])
+    tags=StringField()
+    assignedTo=ReferenceField(User,required=True)
+    rejectedReason=StringField()
+    createdAt=DateTimeField(default=datetime.now())
+    updatedAt=DateTimeField()
+
+class IssueStatusHistory(Document):
+    id=StringField(primary_key=True, default= lambda: str(uuid4()))
+    issue=ReferenceField(Issue,required=True)
+    prvStatus=StringField(required=True)
+    nextStatus=StringField(required=True)
+    updatedBy=ReferenceField(User,required=True)
+    resolvedAt=DateTimeField()
+    updatedAt=DateTimeField()
+
+class Notifications(Document):
+    id=StringField(primary_key=True, default= lambda: str(uuid4()))
+    user=ReferenceField(User,required=True)
+    issue=ReferenceField(Issue,required=True)
+    message=StringField(required=True)
+    isViewed=BooleanField(default=False)
+    createdAt=DateTimeField(default=datetime.now())
+    updatedAt=DateTimeField()
