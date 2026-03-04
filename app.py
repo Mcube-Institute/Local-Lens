5b7bdb0de376227dee18dfef18380d6c003e5ad6
Akvk33 <akvenky33@gmail.com>
2026-03-04 11:43:25 +0530

    fix mail config

--- app.py
+++ app.py
@@ -22,10 +22,11 @@ app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
-app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT"))
-app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS") == "True"
+app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT",587))
+app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS", "True").lower() == "true"
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+print("MAIL CONFIG:", app.config["MAIL_SERVER"], app.config["MAIL_PORT"])
 
 try:
     connect(host=os.getenv("MONGO_URI"))
